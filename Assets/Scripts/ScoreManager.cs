3e7590d19077e3e5bb8f45b2aede63f5b73046ce
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-05 15:15:22 +0300

    Create score manager and score panel

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+using UnityEngine.UI;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+    private int currentScore = 0;
+    private int counterValue = 0;
+    private int increment = 5;
+
+    public Text scoreText;
+
+    private void Start() => UpdateScoreText(currentScore);
+
+    private IEnumerator CountScoreRoutine()
+    {
+        int iterations = 0;
+
+        while (counterValue < currentScore && iterations < 1000000)
+        {
+            counterValue += increment;
+            UpdateScoreText(counterValue);
+            iterations++;
+
+            yield return null;
+        }
+
+        counterValue = currentScore;
+        UpdateScoreText(currentScore);
+    }
+
+    public void UpdateScoreText(int scoreValue)
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = scoreValue.ToString();
+        }
+    }
+
+    public void AddScore(int value)
+    {
+        currentScore += value;
+        StartCoroutine(CountScoreRoutine());
+    }
+}
