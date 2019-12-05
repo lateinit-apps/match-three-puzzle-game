d8b560ed510dd7cb68e30a40f95922b35a0eb0f7
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-05 15:03:17 +0300

    Implement singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
