ef0a1bf100d6a2e49b8fae4e2d5b5d463fd35946
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-11 13:33:55 +0300

    Add time bonus component to all prefabs

--- Assets/Scripts/TimeBonus.cs
+++ Assets/Scripts/TimeBonus.cs
@@ -3,5 +3,37 @@
 [RequireComponent(typeof(GamePiece))]
 public class TimeBonus : MonoBehaviour
 {
+    [Range(0, 5)]
     public int bonusValue = 5;
+
+    [Range(0f, 1f)]
+    public float chanceForBonus = 0.1f;
+
+    public GameObject bonusGlow;
+    public GameObject ringGlow;
+
+    private void Start()
+    {
+        float random = Random.Range(0f, 1f);
+
+        if (random > chanceForBonus)
+        {
+            bonusValue = 0;
+        }
+
+        SetActive(bonusValue != 0);
+    }
+
+    private void SetActive(bool state)
+    {
+        if (bonusGlow != null)
+        {
+            bonusGlow.SetActive(state);
+        }
+
+        if (ringGlow != null)
+        {
+            ringGlow.SetActive(state);
+        }
+    }
 }
